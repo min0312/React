229ea9490fcf34e7c13059d3819703edbd0d418f
min0312 <mkm6192@naver.com>
2022-10-05 17:12:02 +0900

    리액트 시계

---
+++ project/app01/src/components/MyTick.jsx
@@ -0,0 +1,12 @@
+import './My.css';
+
+function MyTick() {
+  console.log("App 컴포넌트")
+  return (
+    <h2>
+      현재 시간
+    </h2>
+  );
+}
+
+export default MyTick;
--- project/app02/src/App.js
+++ project/app02/src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import MyTick from './components/MyTick';
+import MyTick from './mycom/MyTick';
 
 function App() {
-  console.log("App 컴포넌트")
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
       </header>
       <MyTick />
     </div>
